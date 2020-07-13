053466ac55c7433d296d18f4d7947efb2218e09c
Liyuan He <woaihkx@gmail.com>
2020-07-12 22:01:23 -0500

    assignment-13-todo2: completed

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 function onReady() {
   let toDos = [];
+  let id = 0;
   const addToDoForm = document.getElementById("addToDoForm");
   function createNewToDo() {
     const newToDoText = document.getElementById("newToDoText");
     if (!newToDoText.value) { return; }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
     });
+    id += 1;
     newToDoText.value = "";
     renderTheUI();
   }
@@ -22,6 +25,18 @@ function onReady() {
       newLi.textContent = toDo.title;
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+
+      let button = document.createElement("button");
+      let textNode = document.createTextNode("Delete");
+      button.appendChild(textNode);
+      newLi.appendChild(button);
+      button.addEventListener("click", event => {
+        //delete toDos[toDo.id];
+        toDos = toDos.filter(eachToDo => eachToDo.id != toDo.id);
+        newLi.parentNode.removeChild(newLi);
+        renderTheUI();
+      });
+
     });
   }
 
